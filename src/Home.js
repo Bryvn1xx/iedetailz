9fd5cf0d823395692e2812076a8e8c400547ba8d
Bryan <bryannsanchez1234@gmail.com.com>
2023-06-08 05:37:05 -0700

    update

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,16 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="home-container">
-
       <div className="featured-image"></div>
 
       <section className="about-section">
-        <h2>About Us</h2>
+        <h2>Rancho Cucamonga, CA</h2>
         <p>
-          IEDetailz is a mobile detailing service based in Rancho Cucamonga, CA. We specialize in providing comprehensive interior and exterior detailing services, including exterior polishing and ceramic coating. Our team of experienced professionals is dedicated to delivering top-quality service and exceptional results to our customers.
-        </p>
+        Welcome to iedetailz, your premier mobile detailing service in Rancho Cucamonga, CA. We specialize in comprehensive interior and exterior detailing, delivering top-quality results that will make your vehicle shine like never before.
+
+Experience the difference of our advanced ceramic coating, providing ultimate protection and an irresistible shine. From paint correction to enhancing the beauty of your car, we've got you covered.
+
+Join the growing community of car enthusiasts in the Inland Empire who trust us with their prized possessions. Discover the secret to automotive perfection today.        </p>
       </section>
 
       <section className="services-section">
@@ -19,17 +21,25 @@ const Home = () => {
           <div className="service-box mobile-wash">
             <h4 className="service-label">Mobile Wash</h4>
             <div className="service-image blue-box"></div>
-            <p className="service-description">Keep your car clean and shining with our convenient mobile wash service. We come to you and provide a thorough wash, including exterior cleaning and interior vacuuming.</p>
+            <p className="service-description">
+              Keep your car looking pristine with our convenient and eco-friendly mobile wash service. We bring our expertise directly to you, providing a thorough exterior cleaning and interior vacuuming. Say goodbye to dirt and hello to a spotless vehicle.
+            </p>
           </div>
+
           <div className="service-box paint-restoration">
             <h4 className="service-label">Paint Restoration / Polish</h4>
             <div className="service-image blue-box"></div>
-            <p className="service-description">Restore the luster of your car's paint with our paint restoration and polishing service. Our experts will remove swirls, scratches, and imperfections, leaving your car with a smooth and glossy finish.</p>
+            <p className="service-description">
+              Restore the beauty and shine of your car's paint with our expert paint restoration and polishing service. Our skilled technicians will eliminate swirls, scratches, and imperfections, leaving your vehicle with a flawless and glossy finish.
+            </p>
           </div>
+
           <div className="service-box ceramic-coating">
             <h4 className="service-label">Ceramic Coating</h4>
             <div className="service-image blue-box"></div>
-            <p className="service-description">Give your car long-lasting protection and a brilliant shine with our ceramic coating service. Our advanced ceramic coatings create a strong barrier against UV rays, dirt, and contaminants.</p>
+            <p className="service-description">
+              Give your car the ultimate protection and an incredible shine with our advanced ceramic coating service. Our premium coatings create a strong barrier against UV rays, dirt, and contaminants, ensuring long-lasting beauty and effortless maintenance.
+            </p>
           </div>
         </div>
       </section>
