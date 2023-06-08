4fbb1e229648a2b4e7596c2d20ae24a5daf481c4
Bryan <bryannsanchez1234@gmail.com.com>
2023-06-08 04:17:25 -0700

    nav bar update, and deployment

--- src/Nav.js
+++ src/Nav.js
@@ -3,11 +3,18 @@ import React from 'react';
 const Nav = () => {
   return (
     <nav className="nav">
-      <ul className="nav-list">
-        <li><a href="/">Home</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/about">About</a></li>
-      </ul>
+      <div className="nav-brand">
+        <h3>
+          iedetailz
+        </h3>
+      </div>
+      <div>
+        <ul className="nav-list">
+          <li><a href="/">Home</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/about">About</a></li>
+        </ul>
+      </div>
     </nav>
   );
 };
