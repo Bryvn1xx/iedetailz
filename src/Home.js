62fdac61b967ad2ddfa29b8dfa566e58a42b531a
Bryan <bryannsanchez1234@gmail.com.com>
2023-06-08 05:03:04 -0700

    some changes

--- src/Home.js
+++ src/Home.js
@@ -20,19 +20,19 @@ const Home = () => {
         <h2>Our Services</h2>
         <div className="service-boxes">
           <div className="service-box mobile-wash">
+            <h4 className="service-label">Mobile Wash</h4>
             <div className="service-image blue-box"></div>
-            <h3>Mobile Wash</h3>
-            <p>Keep your car clean and shining with our convenient mobile wash service. We come to you and provide a thorough wash, including exterior cleaning and interior vacuuming.</p>
+            <p className="service-description">Keep your car clean and shining with our convenient mobile wash service. We come to you and provide a thorough wash, including exterior cleaning and interior vacuuming.</p>
           </div>
           <div className="service-box paint-restoration">
+            <h4 className="service-label">Paint Restoration / Polish</h4>
             <div className="service-image blue-box"></div>
-            <h3>Paint Restoration / Polish</h3>
-            <p>Restore the luster of your car's paint with our paint restoration and polishing service. Our experts will remove swirls, scratches, and imperfections, leaving your car with a smooth and glossy finish.</p>
+            <p className="service-description">Restore the luster of your car's paint with our paint restoration and polishing service. Our experts will remove swirls, scratches, and imperfections, leaving your car with a smooth and glossy finish.</p>
           </div>
           <div className="service-box ceramic-coating">
+            <h4 className="service-label">Ceramic Coating</h4>
             <div className="service-image blue-box"></div>
-            <h3>Ceramic Coating</h3>
-            <p>Give your car long-lasting protection and a brilliant shine with our ceramic coating service. Our advanced ceramic coatings create a strong barrier against UV rays, dirt, and contaminants.</p>
+            <p className="service-description">Give your car long-lasting protection and a brilliant shine with our ceramic coating service. Our advanced ceramic coatings create a strong barrier against UV rays, dirt, and contaminants.</p>
           </div>
         </div>
       </section>
--- src/Nav.js
+++ src/Nav.js
@@ -10,9 +10,9 @@ const Nav = () => {
       </div>
       <div>
         <ul className="nav-list">
-          <li><a href="/">Home</a></li>
+          {/* <li><a href="/">Home</a></li> */}
           <li><a href="/contact">Contact</a></li>
-          <li><a href="/about">About</a></li>
+          {/* <li><a href="/about">About</a></li> */}
         </ul>
       </div>
     </nav>
